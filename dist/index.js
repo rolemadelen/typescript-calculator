5b79086049acafcd4c8c6b8552ff21e28fa4bb52
Madelen Yoo <rolemadelen@tuta.io>
2022-03-20 22:00:27 +0900

    BEM methodology appiled + basic functionality added
    Modifed current CSS to apple BEM methodology.
    
    Added basic functionality:
    - sum
    - difference
    - product
    - division
    - percent
    - sign conversion
    
    Need to work on updating the value when clicking different operators.
    
    Signed-off-by: Jii Yoo <jiiyoo@pm.me>
    Signed-off-by: Madelen Yoo <rolemadelen@pm.me>
    Signed-off-by: Madelen Yoo <rolemadelen@tuta.io>

--- dist/index.js
+++ dist/index.js
@@ -1,7 +1,138 @@
 "use strict";
-function sum(x, y) {
-    return x + y;
-}
-const message = "hello";
-console.log(message);
-console.log(sum(1, 2));
+window.onload = () => {
+    const buttons = document.querySelectorAll("button");
+    const screen = document.querySelector("input[name='screen']");
+    let operandA = null;
+    let operandB = null;
+    let screenValue = '';
+    let isOperatorSelected = false;
+    let isDecimal = false;
+    let selectedOperator = '';
+    function clearScreen() {
+        screen.setAttribute('value', '0');
+        operandA = null;
+        operandB = null;
+        screenValue = '';
+        isOperatorSelected = false;
+        isDecimal = false;
+        selectedOperator = '';
+    }
+    function invertSign() {
+        let num = Number(screen.value);
+        num *= -1;
+        screen.setAttribute('value', String(num));
+    }
+    function percentify() {
+        const num = Number(screen.value) / 100;
+        screenValue = String(num);
+        screen.setAttribute('value', screenValue);
+    }
+    function add(op) {
+        if (operandA == null) {
+            operandA = Number(screenValue);
+        }
+        else {
+            operandB = Number(screenValue);
+            operandA += operandB;
+            screen.setAttribute('value', String(operandA));
+        }
+        selectedOperator = op;
+        isOperatorSelected = true;
+    }
+    function subtract(op) {
+        if (operandA == null) {
+            operandA = Number(screenValue);
+        }
+        else {
+            operandB = Number(screenValue);
+            operandA -= operandB;
+            screen.setAttribute('value', String(operandA));
+        }
+        selectedOperator = op;
+        isOperatorSelected = true;
+    }
+    function multiply(op) {
+        if (operandA == null) {
+            operandA = Number(screenValue);
+        }
+        else {
+            operandB = Number(screenValue);
+            operandA *= operandB;
+            screen.setAttribute('value', String(operandA));
+        }
+        selectedOperator = op;
+        isOperatorSelected = true;
+    }
+    function divide(op) {
+        if (operandA == null) {
+            operandA = Number(screenValue);
+        }
+        else {
+            operandB = Number(screenValue);
+            operandA /= operandB;
+            screen.setAttribute('value', String(operandA));
+        }
+        selectedOperator = op;
+        isOperatorSelected = true;
+    }
+    function process(op) {
+        console.log("screenValue: ", screenValue);
+        if (op === "C") {
+            clearScreen();
+        }
+        else if (op === "+/−") {
+            invertSign();
+        }
+        else if (op === "%") {
+            percentify();
+        }
+        else if (op === "+") {
+            add(op);
+        }
+        else if (op === "−") {
+            subtract(op);
+        }
+        else if (op === "×") {
+            multiply(op);
+        }
+        else if (op === "÷") {
+            divide(op);
+        }
+        else if (op === ".") {
+            if (!isDecimal) {
+                isDecimal = true;
+            }
+        }
+        else if (op === "=") {
+            if (selectedOperator !== '') {
+                let temp = selectedOperator;
+                selectedOperator = '';
+                process(temp);
+            }
+        }
+    }
+    for (let i = 0; i < buttons.length; ++i) {
+        buttons[i].addEventListener("click", (e) => {
+            let target = e.target;
+            let value = Number(target.textContent);
+            if (value >= 0 && value < 10) {
+                let currentValue = screen.value;
+                if (currentValue == "0" || isOperatorSelected) {
+                    currentValue = "";
+                    isOperatorSelected = false;
+                }
+                if (isDecimal) {
+                    currentValue += ".";
+                }
+                currentValue = currentValue + target.textContent;
+                screenValue = currentValue;
+                console.log(screenValue);
+                screen.setAttribute('value', currentValue);
+            }
+            else {
+                let operator = target.textContent;
+                process(operator);
+            }
+        });
+    }
+};
--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,146 @@
-function sum(x: number, y:number): number {
-    return x+y;
-}
+window.onload = () => {
+    const buttons: NodeList = document.querySelectorAll("button");
+    const screen: HTMLInputElement = document.querySelector("input[name='screen']") as HTMLInputElement;
+    let operandA: number | null = null;
+    let operandB: number | null = null;
+    let screenValue: string | null = '';
+    let isOperatorSelected: boolean = false;
+    let isDecimal: boolean = false;
+    let selectedOperator: string = '';
 
-const message: string = "hello";
-console.log(message);
+    function clearScreen(): void {
+        screen.setAttribute('value', '0');
+        operandA = null;
+        operandB = null;
+        screenValue = '';
+        isOperatorSelected = false;
+        isDecimal = false;
+        selectedOperator = '';
+    }
 
-console.log(sum(1,2));
+    function invertSign(): void {
+        let num: number = Number(screen.value);
+        num *= -1;
+        screen.setAttribute('value', String(num));
+    }
+
+    function percentify(): void {
+        const num: number = Number(screen.value) / 100;
+        screenValue = String(num);
+        screen.setAttribute('value', screenValue);
+    }
+
+    function add(op: string): void {
+        if(operandA == null) {
+            operandA = Number(screenValue);
+        } else {
+            operandB = Number(screenValue);
+            operandA += operandB;
+            screen.setAttribute('value', String(operandA));
+        }
+
+        selectedOperator = op;
+        isOperatorSelected = true;
+    }
+
+    function subtract(op: string): void {
+        if(operandA == null) {
+            operandA = Number(screenValue);
+        } else {
+            operandB = Number(screenValue);
+            operandA -= operandB;
+            screen.setAttribute('value', String(operandA));
+        }
+
+        selectedOperator = op;
+        isOperatorSelected = true;
+    }
+
+    function multiply(op: string): void {
+        if(operandA == null) {
+            operandA = Number(screenValue);
+        } else {
+            operandB = Number(screenValue);
+            operandA *= operandB;
+            screen.setAttribute('value', String(operandA));
+        }
+
+        selectedOperator = op;
+        isOperatorSelected = true;   
+    }
+
+    function divide(op: string): void {
+        if(operandA == null) {
+            operandA = Number(screenValue);
+        } else {
+            operandB = Number(screenValue);
+            operandA /= operandB;
+            screen.setAttribute('value', String(operandA));
+        }
+
+        selectedOperator = op;
+        isOperatorSelected = true;   
+    }
+
+    function process(op: string | null): void {
+        console.log("screenValue: ", screenValue);
+        if(op === "C") { 
+            clearScreen(); 
+        }
+        else if(op === "+/−") { 
+            invertSign(); 
+        }
+        else if(op === "%") { 
+            percentify(); 
+        }
+        else if(op === "+") { 
+            add(op);
+        }
+        else if(op === "−") {
+            subtract(op);
+        }
+        else if(op === "×") {
+            multiply(op);
+        }
+        else if(op === "÷") {
+            divide(op);
+        }
+        else if(op === ".") {
+            if(!isDecimal) {
+                isDecimal = true;
+            }
+        }
+        else if(op === "=") {
+            if(selectedOperator !== '') {
+                let temp: string = selectedOperator;
+                selectedOperator = '';
+                process(temp);
+            }
+        }
+    }
+
+    for (let i = 0; i < buttons.length; ++i) {
+        buttons[i].addEventListener("click", (e: Event) => {
+            let target: HTMLElement = e.target as HTMLElement;
+            let value: number = Number(target.textContent);
+            if(value >= 0 && value < 10) {
+                let currentValue: string = screen.value;
+                if (currentValue == "0" || isOperatorSelected ) { 
+                    currentValue = ""; 
+                    isOperatorSelected = false;
+                }
+
+                if(isDecimal) {
+                    currentValue += ".";
+                }
+                currentValue = currentValue + target.textContent;
+                screenValue = currentValue;
+                console.log(screenValue);
+                screen.setAttribute('value', currentValue);
+            } else {
+                let operator: string | null = target.textContent;
+                process(operator);
+            }
+        })
+    }
+};
